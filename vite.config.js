307d5aab7f96710abb18ecac44b0ad16f36db7ba
kuku <polorbear520@gmail.com>
2025-06-08 20:08:00 +0800

    fix: 🐛 camp-4 設定 base 處理靜態資源路徑錯誤問題

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  // base 的寫法:
-  // base: '/Repository 的名稱/'
-  base: "",
-  plugins: [vue(), tailwindcss(), "@tailwindcss/postcss"],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/2025-web-camp/" : "/",
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
